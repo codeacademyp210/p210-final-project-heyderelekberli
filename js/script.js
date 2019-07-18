734573daa4244a7a5c9cf7a4560ca51c07707a88
- <HeydarRa@code.edu.az>
2019-07-18 19:12:15 +0400

    done

--- js/script.js
+++ js/script.js
@@ -6,3 +6,15 @@ function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
 }
+
+// document.querySelector(".base-image").addEventListener("mouseover", function () {
+//   let header = document.querySelector(".user");
+//   header.style.color = ("#fd602c");
+//   console.log(header);
+// })
+
+// document.querySelector(".bulb").addEventListener("mouseout", function () {
+//   let header = document.querySelector(".user");
+//   header.style.color = ("black");
+//   console.log(header);
+// })
