912686181903ce74b3d948cabc461658fce3e4c1
- <HeydarRa@code.edu.az>
2019-07-22 22:05:15 +0400

    done

--- js/script.js
+++ js/script.js
@@ -1,20 +1,49 @@
+"use strict";
+
 function openNav() {
   document.getElementById("mySidebar").style.width = "300px";
 }
 
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
+  document.getElementById("main").style.marginLeft = "0";
 }
 
-// document.querySelector(".base-image").addEventListener("mouseover", function () {
-//   let header = document.querySelector(".user");
-//   header.style.color = ("#fd602c");
-//   console.log(header);
-// })
 
-// document.querySelector(".bulb").addEventListener("mouseout", function () {
-//   let header = document.querySelector(".user");
-//   header.style.color = ("black");
-//   console.log(header);
-// })
+
+(function () {
+
+  // Create input element for testing
+  var inputs = document.createElement('input');
+
+  // Create the supports object
+  var supports = {};
+
+  supports.autofocus = 'autofocus' in inputs;
+  supports.required = 'required' in inputs;
+  supports.placeholder = 'placeholder' in inputs;
+
+  // Fallback for autofocus attribute
+  if (!supports.autofocus) {
+
+  }
+
+  // Fallback for required attribute
+  if (!supports.required) {
+
+  }
+
+  // Fallback for placeholder attribute
+  if (!supports.placeholder) {
+
+  }
+
+  // // Change text inside send button on submit
+  // var send = document.getElementById('contact-submit');
+  // if (send) {
+  //   send.onclick = function () {
+  //     this.innerHTML = '...Sending';
+  //   }
+  // }
+
+})();
