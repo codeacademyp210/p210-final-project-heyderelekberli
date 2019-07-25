511e35b6f972f022fbd02b92db4034221ae6351a
- <HeydarRa@code.edu.az>
2019-07-25 21:01:10 +0400

    commit

--- FinalProject/js/script.js
+++ FinalProject/js/script.js
@@ -9,8 +9,6 @@ function closeNav() {
   document.getElementById("main").style.marginLeft = "0";
 }
 
-
-
 (function () {
 
   // Create input element for testing
@@ -37,13 +35,72 @@ function closeNav() {
   if (!supports.placeholder) {
 
   }
-
-  // // Change text inside send button on submit
-  // var send = document.getElementById('contact-submit');
-  // if (send) {
-  //   send.onclick = function () {
-  //     this.innerHTML = '...Sending';
-  //   }
-  // }
-
 })();
+
+//Fixed Button
+
+function fadeIn(elem, speed) {
+  if (elem.style.opacity) {
+    elem.style.opacity = 0;
+    elem.style.display = "block";
+  }
+  var fadeInInterval = setInterval(function () {
+    elem.style.opacity = Number(elem.style.opacity) + 0.02;
+    if (elem.style.opacity >= 1) {
+      clearInterval(fadeInInterval);
+    }
+  }, speed);
+}
+
+function fadeOut(elem, speed) {
+  if (!elem.style.opacity) {
+    elem.style.opacity = 1;
+  }
+  var fadeOutInterval = setInterval(function () {
+    elem.style.opacity = Number(elem.style.opacity) - 0.02;
+    if (elem.style.opacity <= 0) {
+      clearInterval(fadeOutInterval);
+      elem.style.display = "none";
+    }
+  }, speed);
+}
+
+window.onscroll = function () { scrollFunction() };
+
+var isScrollUpDisplayed = false;
+var scrollUpElem = document.querySelector(".scroll-top");
+
+scrollUpElem.addEventListener("click", function () {
+  var upInterval = setInterval(function () {
+    if (document.documentElement.scrollTop <= 0) {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+      clearInterval(upInterval);
+    } else {
+      document.body.scrollTop -= 20;
+      document.documentElement.scrollTop -= 20;
+    }
+
+  }, 1);
+
+});
+
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    if (!isScrollUpDisplayed) {
+      fadeIn(scrollUpElem, 10);
+      isScrollUpDisplayed = true;
+    }
+  } else {
+    if (isScrollUpDisplayed) {
+      fadeOut(scrollUpElem, 10);
+      isScrollUpDisplayed = false;
+    }
+  }
+}
+
+var section = document.querySelector(".simple-section");
+section.addEventListener("click", function () {
+  fadeOut(tabs[0], 20)
+});
