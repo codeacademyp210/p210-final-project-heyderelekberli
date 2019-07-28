73bb9a28674ca94ae162fb57430e707a5d6fd47d
- <HeydarRa@code.edu.az>
2019-07-29 02:35:46 +0400

    done

---
+++ FinalProject/FinalProject/Controllers/MainController.cs
@@ -0,0 +1,21 @@
+﻿using FinalProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinalProject.Controllers
+{
+    public class MainController : Controller
+    {
+        protected MoviesEntities db;
+        protected Slider Slider;
+
+        protected MainController()
+        {
+            db = new MoviesEntities();
+            Slider = db.Sliders.First();
+        }
+    }
+}
---
+++ FinalProject/FinalProject/Controllers/SeansController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinalProject.Controllers
+{
+    public class SeansController : Controller
+    {
+        // GET: Seans
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SeansProfile()
+        {
+            return View();
+        }
+        public ActionResult SeansTable()
+        {
+            return View();
+        }
+    }
+}
